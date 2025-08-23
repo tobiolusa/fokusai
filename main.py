539565e11bea8a7b74a3de074dd0a1b1488b7a12
thepeterstudio <thepeterstudio@outlook.com>
2025-08-24 00:47:51 +0100

    add : chat frontend updated

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"message" : "fokusai"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/chat', response_class=HTMLResponse)
+async def chat(request : Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html",
+
+    )

584a43400be6506c4dcbd4a9b2c987f34ed63f94
thepeterstudio <thepeterstudio@outlook.com>
2025-09-20 19:01:19 +0100

    added model = dialogpt

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
+
+
+# Load API key from environment
+HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
+if not HUGGINGFACE_API_KEY:
+    raise ValueError("HUGGINGFACE Token not set")
+
+MODEL_NAME = "microsoft/DialoGPT-medium"
+HF_API_URL = 'https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium'
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -12,5 +26,4 @@ templates = Jinja2Templates(directory="templates")
 async def chat(request : Request):
     return templates.TemplateResponse(
         request=request, name="index.html",
-
     )

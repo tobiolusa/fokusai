0ff8481376b92ac6c437b189f6221073e5cc938a
thepeterstudio <thepeterstudio@outlook.com>
2025-09-20 23:14:41 +0100

    token : access token added

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi import HTTPException
+from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
@@ -11,12 +14,18 @@ app = FastAPI()
 
 
 # Load API key from environment
-HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
-if not HUGGINGFACE_API_KEY:
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+if not HUGGINGFACE_TOKEN:
     raise ValueError("HUGGINGFACE Token not set")
 
 MODEL_NAME = "microsoft/DialoGPT-medium"
 HF_API_URL = 'https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium'
+
+
+## Model 
+
+class Question(BaseModel):
+    question : str
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -27,3 +36,36 @@ async def chat(request : Request):
     return templates.TemplateResponse(
         request=request, name="index.html",
     )
+
+@app.post('/')
+async def question(q : Question):
+    if not q.question:
+        raise HTTPException(status_code=400, detail="Question cant be empty")
+    payload = {
+        "input" : 'q.question',
+        "parameters" : {
+            "max_length" : 200,
+            "num_return_sequences" : 1
+        }
+    }
+
+    headers = {
+        "Authorization" : f"Bearer {HUGGINGFACE_TOKEN}",
+        "Conten-Type" : "application/json"
+    }
+    
+    try : 
+        response = requests.post(HF_API_URL,headers=headers, json=payload)
+        response.raise_for_status()
+        result = response.json()
+        
+        if isinstance(result, list) and result :
+            answer = result[0].get("generated_text", "No response genereated")
+        else:
+            answer = result.get("generated_text", "No response genereated")
+        result 
+        {"answer" : answer.strip()}
+
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail = f"Error communicating with AI API : {str(e)}")  
+    
